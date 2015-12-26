c82f3c770bfee2c1c900cda780a0e72b3ecb8307
Nikola Novakovic <nikola.novakovic9@gmail.com>
2015-12-26 16:15:36 +0100

    Add routing store picker to the app

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,11 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
+
 /*
  *  App
  */
@@ -74,11 +78,19 @@ var Inventory = React.createClass({
  */
 
 var StorePicker = React.createClass({
+  mixins: [History],
+  goToStore: function(event) {
+    event.preventDefault();
+
+    var storeId = this.refs.storeId.value;
+  
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render: function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
       </form>
     )
