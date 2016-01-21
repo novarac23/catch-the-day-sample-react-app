549d259f451754b755106d39659c8b5377e90b92
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-01-21 21:45:41 +0100

    Start moving es5 components to es6 classes

--- scripts/components/NotFound.js
+++ scripts/components/NotFound.js
@@ -4,13 +4,11 @@
 
 import React from 'react';
 
-var NotFound = React.createClass({
-  render: function () {
-    return (
-      <h1>Not Found!</h1> 
-    ) 
+class NotFound extends React.Component {
+  render() {
+    return <h1>Not Found!</h1> 
   }
-});
+}
 
 export default NotFound;
 
--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -5,17 +5,20 @@
 import React from 'react';
 import { Navigation, History } from 'react-router'
 import h from '../helpers'
+import reactMixin from 'react-mixin';
 
-var StorePicker = React.createClass({
-  mixins: [History],
-  goToStore: function(event) {
+class StorePicker extends React.Component {
+  mixins: [History]
+
+  goToStore(event) {
     event.preventDefault();
 
     var storeId = this.refs.storeId.value;
   
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render: function() {
+  }
+
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -24,6 +27,8 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
